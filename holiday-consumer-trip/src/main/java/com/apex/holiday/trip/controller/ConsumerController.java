af516b3f0ae467d35ad10e62581d40fdf33595da
yangcheng <1430478198@qq.com>
2018-04-10 17:32:18 +0800

    调试消费端

--- holiday-consumer-trip/src/main/java/com/apex/holiday/trip/controller/ConsumerController.java
+++ holiday-consumer-trip/src/main/java/com/apex/holiday/trip/controller/ConsumerController.java
@@ -3,9 +3,8 @@ package com.apex.holiday.trip.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import com.apex.holiday.api.TestService;
 import com.apex.holiday.dto.HolidayTestDto;
 
@@ -16,22 +15,22 @@ import com.apex.holiday.dto.HolidayTestDto;
  * @version: v1.0
  */
 @RestController
-public class TestController {
+public class ConsumerController {
 
     @Autowired
     private TestService holidayTestService;
 
     /**
      * @author: yangcheng
-     * @createTime: 2018年4月2日 下午3:50:23
-     * @return String
+     * @createTime: 2018年4月2日 上午11:55:58
+     * @return ModelAndView
      */
-    @RequestMapping(value = "test", method = RequestMethod.GET)
-    @ResponseBody
-    public String test() {
-        List<HolidayTestDto> list = holidayTestService.listCity();
-        System.out.println(list);
-        return "test";
+    @RequestMapping(value = "/home")
+    public ModelAndView home() {
+        ModelAndView mv = new ModelAndView("home");
+        List<HolidayTestDto> dtos = holidayTestService.listCity();
+        mv.addObject("cityData", dtos);
+        return mv;
     }
 
 }
---
+++ holiday-consumer-trip/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,39 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.util.UUID" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page isELIgnored="false"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD//XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <title>holiday-cunsumer Home</title>
+    <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/jscript"></script>
+</head>
+<body>
+  <br/>
+    <div class="box-123">
+      <table class="table table-bordered">
+         <tr>
+             <th>城市编号</th>
+             <th>城市名称</th>
+             <th>旅程價格</th>
+             <th>添加時間</th>
+          </tr>
+        <tbody id="cityCols">
+		<c:forEach items="${cityData}" var="ct">
+			<tr id ='${ct.appId}'>
+				<td>${ct.appId}</td>
+				<td>${ct.city}</td>
+				<td>${ct.price}</td>
+				<td>${ct.addTime}</td>
+			</tr>
+		</c:forEach>
+	   </tbody>        
+     </table>
+   </div>
+</body>
+</html>
